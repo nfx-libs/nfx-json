9d09292f6ed6b989511d1fc24829e5efe0188075
Ronan F․ <ronan.fesselier@etik.com>
2026-02-11 07:06:56 +0100

    chore(deps): bump nfx-stringbuilder and nfx-stringutils dependencies to latest versions

--- cmake/nfxJsonDependencies.cmake
+++ cmake/nfxJsonDependencies.cmake
@@ -18,11 +18,11 @@ set(CMAKE_FIND_QUIETLY      ON     )
 # Dependency versions
 #----------------------------------------------
 
-set(NFX_JSON_DEPS_NFX_STRINGUTILS_VERSION    "0.6.1")
-set(NFX_JSON_DEPS_NFX_STRINGBUILDER_VERSION  "0.5.0")
-set(NFX_JSON_DEPS_NFX_CONTAINERS_VERSION     "0.4.1")
-set(NFX_JSON_DEPS_NFX_HASHING_VERSION        "0.2.0")
-set(NFX_JSON_DEPS_NFX_RESOURCE_VERSION       "1.1.0")
+set(NFX_JSON_DEPS_NFX_CONTAINERS_VERSION    "0.4.1")
+set(NFX_JSON_DEPS_NFX_HASHING_VERSION       "0.2.0")
+set(NFX_JSON_DEPS_NFX_RESOURCE_VERSION      "1.1.0")
+set(NFX_JSON_DEPS_NFX_STRINGBUILDER_VERSION "0.6.0")
+set(NFX_JSON_DEPS_NFX_STRINGUTILS_VERSION   "0.6.2")
 
 #----------------------------------------------
 # FetchContent dependencies
@@ -36,55 +36,6 @@ else()
     set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
 endif()
 set(FETCHCONTENT_QUIET OFF)
-
-# --- nfx-stringutils ---
-find_package(nfx-stringutils ${NFX_JSON_DEPS_NFX_STRINGUTILS_VERSION} QUIET)
-if(NOT nfx-stringutils_FOUND)
-    set(NFX_STRINGUTILS_BUILD_TESTS         OFF CACHE BOOL "")
-    set(NFX_STRINGUTILS_BUILD_SAMPLES       OFF CACHE BOOL "")
-    set(NFX_STRINGUTILS_BUILD_BENCHMARKS    OFF CACHE BOOL "")
-    set(NFX_STRINGUTILS_BUILD_DOCUMENTATION OFF CACHE BOOL "")
-    set(NFX_STRINGUTILS_INSTALL_PROJECT     OFF CACHE BOOL "")
-    set(NFX_STRINGUTILS_PACKAGE_SOURCE      OFF CACHE BOOL "")
-    set(NFX_STRINGUTILS_PACKAGE_ARCHIVE     OFF CACHE BOOL "")
-    set(NFX_STRINGUTILS_PACKAGE_DEB         OFF CACHE BOOL "")
-    set(NFX_STRINGUTILS_PACKAGE_RPM         OFF CACHE BOOL "")
-
-    FetchContent_Declare(
-        nfx-stringutils
-            GIT_REPOSITORY https://github.com/nfx-libs/nfx-stringutils.git
-            GIT_TAG        ${NFX_JSON_DEPS_NFX_STRINGUTILS_VERSION}
-            GIT_SHALLOW    TRUE
-            EXCLUDE_FROM_ALL
-    )
-    FetchContent_MakeAvailable(nfx-stringutils)
-    set(NFX_STRINGUTILS_INCLUDE_DIR "${nfx-stringutils_SOURCE_DIR}/include" CACHE INTERNAL "nfx-stringutils include directory")
-endif()
-
-# --- nfx-stringbuilder ---
-find_package(nfx-stringbuilder ${NFX_JSON_DEPS_NFX_STRINGBUILDER_VERSION} QUIET)
-if(NOT nfx-stringbuilder_FOUND)
-    set(NFX_STRINGBUILDER_BUILD_STATIC        ON  CACHE BOOL "")
-    set(NFX_STRINGBUILDER_BUILD_SHARED        OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_BUILD_TESTS         OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_BUILD_SAMPLES       OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_BUILD_BENCHMARKS    OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_BUILD_DOCUMENTATION OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_INSTALL_PROJECT     OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_PACKAGE_SOURCE      OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_PACKAGE_ARCHIVE     OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_PACKAGE_DEB         OFF CACHE BOOL "")
-    set(NFX_STRINGBUILDER_PACKAGE_RPM         OFF CACHE BOOL "")
-
-    FetchContent_Declare(
-        nfx-stringbuilder
-            GIT_REPOSITORY https://github.com/nfx-libs/nfx-stringbuilder.git
-            GIT_TAG        ${NFX_JSON_DEPS_NFX_STRINGBUILDER_VERSION}
-            GIT_SHALLOW    TRUE
-            EXCLUDE_FROM_ALL
-    )
-    FetchContent_MakeAvailable(nfx-stringbuilder)
-endif()
 
 # --- nfx-containers ---
 find_package(nfx-containers ${NFX_JSON_DEPS_NFX_CONTAINERS_VERSION} QUIET)
@@ -146,6 +97,55 @@ if(NOT nfx-resource_FOUND)
             EXCLUDE_FROM_ALL
     )
     FetchContent_MakeAvailable(nfx-resource)
+endif()
+
+# --- nfx-stringbuilder ---
+find_package(nfx-stringbuilder ${NFX_JSON_DEPS_NFX_STRINGBUILDER_VERSION} QUIET)
+if(NOT nfx-stringbuilder_FOUND)
+    set(NFX_STRINGBUILDER_BUILD_STATIC        ON  CACHE BOOL "")
+    set(NFX_STRINGBUILDER_BUILD_SHARED        OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_BUILD_TESTS         OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_BUILD_SAMPLES       OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_BUILD_BENCHMARKS    OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_BUILD_DOCUMENTATION OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_INSTALL_PROJECT     OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_PACKAGE_SOURCE      OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_PACKAGE_ARCHIVE     OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_PACKAGE_DEB         OFF CACHE BOOL "")
+    set(NFX_STRINGBUILDER_PACKAGE_RPM         OFF CACHE BOOL "")
+
+    FetchContent_Declare(
+        nfx-stringbuilder
+            GIT_REPOSITORY https://github.com/nfx-libs/nfx-stringbuilder.git
+            GIT_TAG        ${NFX_JSON_DEPS_NFX_STRINGBUILDER_VERSION}
+            GIT_SHALLOW    TRUE
+            EXCLUDE_FROM_ALL
+    )
+    FetchContent_MakeAvailable(nfx-stringbuilder)
+endif()
+
+# --- nfx-stringutils ---
+find_package(nfx-stringutils ${NFX_JSON_DEPS_NFX_STRINGUTILS_VERSION} QUIET)
+if(NOT nfx-stringutils_FOUND)
+    set(NFX_STRINGUTILS_BUILD_TESTS         OFF CACHE BOOL "")
+    set(NFX_STRINGUTILS_BUILD_SAMPLES       OFF CACHE BOOL "")
+    set(NFX_STRINGUTILS_BUILD_BENCHMARKS    OFF CACHE BOOL "")
+    set(NFX_STRINGUTILS_BUILD_DOCUMENTATION OFF CACHE BOOL "")
+    set(NFX_STRINGUTILS_INSTALL_PROJECT     OFF CACHE BOOL "")
+    set(NFX_STRINGUTILS_PACKAGE_SOURCE      OFF CACHE BOOL "")
+    set(NFX_STRINGUTILS_PACKAGE_ARCHIVE     OFF CACHE BOOL "")
+    set(NFX_STRINGUTILS_PACKAGE_DEB         OFF CACHE BOOL "")
+    set(NFX_STRINGUTILS_PACKAGE_RPM         OFF CACHE BOOL "")
+
+    FetchContent_Declare(
+        nfx-stringutils
+            GIT_REPOSITORY https://github.com/nfx-libs/nfx-stringutils.git
+            GIT_TAG        ${NFX_JSON_DEPS_NFX_STRINGUTILS_VERSION}
+            GIT_SHALLOW    TRUE
+            EXCLUDE_FROM_ALL
+    )
+    FetchContent_MakeAvailable(nfx-stringutils)
+    set(NFX_STRINGUTILS_INCLUDE_DIR "${nfx-stringutils_SOURCE_DIR}/include" CACHE INTERNAL "nfx-stringutils include directory")
 endif()
 
 #----------------------------------------------
